f95ccb05be0c9560fd3d925fb250ff25439cf51a
gogoPAN <panpchen@gmail.com>
2021-03-23 17:31:18 +0800

    data-210323-17:31

--- assets/Script/Game.ts
+++ assets/Script/Game.ts
@@ -7,20 +7,38 @@
 
 const { ccclass, property } = cc._decorator;
 
+const HAND_POS = [
+  cc.v2(-400, -40),
+  cc.v2(-270, 146),
+  cc.v2(190, 106),
+  cc.v2(400, -40),
+  cc.v2(330, -280),
+];
+
+const ORDER_PROGRESS = ["汁", "水", "色", "味", "碳酸氢钠"];
+
 @ccclass
 export default class Game extends cc.Component {
   @property(cc.Prefab)
   tipPrefab: cc.Prefab = null;
+  @property(cc.Node)
+  hand: cc.Node = null;
   public static instance: Game = null;
   private _tip: cc.Node = null;
+  private _selectItemsName: string[] = [];
+  private _curProgressId: number = -1;
 
   onLoad() {
-    // cc.macro.ENABLE_MULTI_TOUCH = false;
-    // const manager = cc.director.getCollisionManager();
-    // manager.enabled = true;
     Game.instance = this;
+    this._curProgressId = 0;
+    this.setHandPos();
   }
-  createTips(content) {
+
+  setHandPos() {
+    this.hand.setPosition(HAND_POS[this._curProgressId]);
+  }
+
+  showTips(content) {
     if (!this._tip) {
       this._tip = cc.instantiate(this.tipPrefab);
     }
@@ -31,4 +49,44 @@ export default class Game extends cc.Component {
     }
     this._tip.parent = cc.director.getScene();
   }
+
+  onClickItem(evt: cc.Toggle, parm) {
+    let clickProgressId = -1;
+    for (let i = 0; i < ORDER_PROGRESS.length; i++) {
+      if (parm.indexOf(ORDER_PROGRESS[i]) != -1) {
+        clickProgressId = i;
+        break;
+      }
+    }
+
+    if (clickProgressId > this._curProgressId) {
+      this.showTips("请按顺序执行哦");
+      if (evt.isChecked) {
+        evt.uncheck();
+      }
+      return;
+    }
+
+    if (clickProgressId < this._curProgressId) {
+      this.showTips("已经做过该步骤了哦");
+      if (!evt.isChecked) {
+        evt.check();
+      } else {
+        evt.uncheck();
+      }
+      return;
+    }
+
+    this._selectItemsName.push(parm);
+
+    this._curProgressId++;
+
+    this.setHandPos();
+  }
+
+  onClickAgain() {}
+
+  onClickSmallGame() {}
+
+  onClickSafeTest() {}
 }
